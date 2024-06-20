7b8c1d15a41614edd7dbd208085114aaeb5abae9
aradhna11 <rajoriyaaradhna963@gmail.com>
2024-06-20 04:14:16 +0000

    new

---
+++ 01_basics/dates_and_time.js
@@ -0,0 +1,18 @@
+ let mydate= new Date()
+ console.log(mydate);
+//  console.log(mydate.toString());
+//  console.log(mydate.toDateString());
+//  console.log(mydate.toLocaleString());
+
+ console.log(typeof  mydate);
+
+//  let myCreatedDate =new Date(2024,0,3)
+//  console.log(myCreatedDate.toDateString());
+
+ let myCreatedDate = new Date("01-12-2024")
+//  console.log(myCreatedDate.toLocaleString());
+
+let mytimestamp =Date.now()
+console.log(mytimestamp);
+
+console.log(myCreatedDate.getTime());
