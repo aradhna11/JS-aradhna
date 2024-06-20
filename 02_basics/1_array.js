0c9a340e2e4edd873019b43819054eedb7ccd2c6
aradhna11 <rajoriyaaradhna963@gmail.com>
2024-06-20 05:02:27 +0000

    new1

---
+++ 02_basics/1_array.js
@@ -0,0 +1,20 @@
+const myArr=[2,35,67,54,3]
+
+console.log(myArr[4]);
+
+// myArr.push(7)
+// myArr.push(9)
+// myArr.pop()
+
+// myArr.unshift(23)
+// myArr.shift()
+
+// console.log(myArr.includes(3));
+// console.log(myArr.indexOf(35));
+
+// const newArr=myArr.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+console.log(myArr);
